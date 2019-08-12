84f872382cf6fad3273565545137347cc6ef689d
mvinicius890 <mvinicius890>
2019-08-11 21:25:02 -0300

    changed github workflow

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          This os volume testing!
+          This is to test github workflow!
         </p>
       </div>
     );
